1e80e4de5ea241745cbbb14f72b07405af0b79de
Duncan Turner <duncanfrederic@gmail.com>
2024-07-22 16:48:32 +1200

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module AucklandWeather-Backend
+
+go 1.22.5
---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func getWeather(w http.ResponseWriter, r *http.Request) {
+	apiKey := "AX0VBcCNEgbPL21wTdOonbtcfKynYTH4"
+	url := "https://api.tomorrow.io/v4/timelines?location=-36.8485,174.7633&fields=temperature,precipitationIntensity&timesteps=1h&units=metric&apikey=" + apiKey
+
+	response, err := http.Get(url)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer response.Body.Close()
+
+	var weatherData map[string]interface{}
+	if err := json.NewDecoder(response.Body).Decode(&weatherData); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(weatherData)
+}
+
+func main() {
+	http.HandleFunc("/api/weather", getWeather)
+	http.ListenAndServe(":8080", nil)
+}
